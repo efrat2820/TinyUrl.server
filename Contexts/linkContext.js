38064b4ee45bd7cfbc8254823251600ba3e1b986
student <student@elkayam.local>
2023-02-08 13:20:42 +0200

    first commit

---
+++ Contexts/linkContext.js
@@ -0,0 +1,41 @@
+import linkModel from "../Models/LinksModel.js"
+
+const linkContext ={
+
+    getAllLink: async()=>{
+        let links = await linkModel.find();
+        return links;
+    } , 
+
+    getLinkById: async(id)=>{
+        const link = await linkModel.findById(id);
+        return link;
+    },
+
+    addLink: async(originalUrl,uniqueName)=>{
+        const newLink = new linkModel({originalUrl,uniqueName});
+        newLink.save();
+        return newLink;
+    },
+
+    updateLink: async(id,link)=>{
+        const updateLink = await linkModel.findByIdAndUpdate(id,link,{new:true});
+        const addLink = await updateLink.save();
+        return addLink;
+    },
+
+    removeLink: async(id)=>{
+        const deleted = await linkModel.findByIdAndRemove(id);
+        return deleted;
+    },
+
+    redirectLink: async(name)=>{
+        console.log('context');
+        const link = await linkModel.findOne({uniqueName:'ultra'});//name
+        const originalUrl=link.originalUrl;
+        console.log('originalUrl',originalUrl)
+        return originalUrl;
+    }
+}
+
+export default linkContext;
---
+++ Contexts/userContext.js
@@ -0,0 +1,33 @@
+import userModel from "../Models/UserModel"
+
+const userContext ={
+
+    getAllUser: async()=>{
+        let users = await userModel.find();
+        return users;
+    } , 
+
+    getUserById: async(id)=>{
+        const user = await userModel.findById(id);
+        return user;
+    },
+
+    addUser: async(name,email,password,links)=>{
+        const newUser = new userModel({name,email,password,links});
+        newUser.save();
+        return newUser;
+    },
+
+    updateUser: async(id,user)=>{
+        const updateUser = await userModel.findByIdAndUpdate(id,user,{new:true});
+        updateUser.save();
+        return updateUser;
+    },
+
+    removeUser: async(id)=>{
+        const deleted = await userModel.findByIdAndRemove(id);
+        return deleted;
+    }
+}
+
+export default userContext;
