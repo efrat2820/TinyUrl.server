f140a16913aa8dd95181d92e995602bf8ff4e1a4
student <student@elkayam.local>
2023-02-09 09:57:39 +0200

    userComplete

---
+++ Controllers/userController.js
@@ -0,0 +1,38 @@
+import context from "../Contexts/userContext"
+
+const UserController = {
+
+    getList: async(req,res)=>{
+        let users = await context.getAllUser();
+        res.send(users);
+    },
+
+    getById: async(req,res)=>{
+        const user = await context.getUserById(req.params.id);
+        res.send(user);
+    },
+
+    add: async(req,res)=>{
+        const {name,email,password} = req.body;
+        console.log('req.body', req.body)
+        const newUser= await context.addUser(name,email,password);
+        res.send(newUser);
+    },
+
+    update: async(req,res)=>{
+        const {id} = req.params;
+        const {name,email,password} = req.body;
+
+        const updateUser = await context.updateUser(id,{name,email,password});
+        res.send(updateUser);
+    },
+
+    delete: async(req,res)=>{
+        const deleted = await context.removeUser(req.params.id);
+        res.send(deleted);
+    },
+
+
+}
+
+export default UserController;
--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -1,1 +1,17 @@
-//להשלים
+import express from 'express'
+import UserController from "../Controllers/userController"
+
+const router = express.Router();
+
+router.get('/',UserController.getList);
+
+router.get('/:id',LinkController.getById);
+
+router.put('/:id',UserController.update);
+
+router.post('/',UserController.add);
+
+router.delete('/:id',UserController.delete);
+
+
+export default router;
